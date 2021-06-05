afef9d7b196c2fa2d2aee0a3eb495506a96a50ca
Mustafaev.Ubaydullo <78743719+Ubaydullo123@users.noreply.github.com>
2021-06-05 14:22:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".swiper-cotainer", {
+  effect: "cube",
+  grabCursor: true,
+  cubeEffect: {
+    shadow: true,
+    sliderShadows: true,
+    shadowEffect: 20,
+    shadowScale: 0.94,
+  },
+});
